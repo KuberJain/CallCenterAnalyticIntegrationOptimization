c3f574f98fb63de1daff41d9dd9d1ec5fba65097
Bichen Zheng <bzheng6@binghamton.edu>
2017-01-03 23:36:52 -0500

    fix hourly and daily call volume aggregation functions

--- visualization/__init__.py
+++ visualization/__init__.py
@@ -4,7 +4,7 @@ import numpy as np
 plt.style.use('ggplot')
 
 # Visualization Functions
-def lineplot(x, y, title, xlabel, ylabel, line_width):
+def lineplot(dir, x, y, title, xlabel, ylabel, line_width):
     plt.plot(x, y, linestyle='-', linewidth=line_width,
                  color='#199DD9')
     plt.xlabel(xlabel)
@@ -13,9 +13,10 @@ def lineplot(x, y, title, xlabel, ylabel, line_width):
     plt.legend()
     plt.tight_layout()
     plt.show()
+    plt.savefig(dir+'/Outputs/visuals/'+title+'.png')
     return None
 
-def barplot(x,y, title, xlabel, ylabel, bar_width):
+def barplot(dir, x,y, title, xlabel, ylabel, bar_width):
     index = np.arange(len(x))
     plt.bar(index, y, width=bar_width, color='#199DD9')
     plt.xlabel(xlabel)
@@ -25,9 +26,10 @@ def barplot(x,y, title, xlabel, ylabel, bar_width):
     plt.legend()
     plt.tight_layout()
     plt.show()
+    plt.savefig(dir+'/Outputs/visuals/'+title+'.png')
     return None
 
-def sidebysidebarplot(x,y,z, title, xlabel, ylabel, zlabel, trendline, bar_width):
+def sidebysidebarplot(dir, x,y,z, title, xlabel, ylabel, zlabel, trendline, bar_width):
     index = np.arange(len(x))
     plt.bar(index-0.15-bar_width/2, y, width=bar_width, color='#A6A8AB', label = ylabel)
     plt.bar(index-0.15+bar_width/2, z, width=bar_width, color='#199DD9', label = zlabel)
@@ -39,4 +41,5 @@ def sidebysidebarplot(x,y,z, title, xlabel, ylabel, zlabel, trendline, bar_width
     plt.legend()
     plt.tight_layout()
     plt.show()
+    plt.savefig(dir+'/Outputs/visuals/'+title+'.png')
     return None
